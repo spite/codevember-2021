cfb1ccf6065d875dc2f26ff2e9f5ab37997280b2
thespite <the.spite@gmail.com>
2021-11-03 01:44:59 +0000

    Create palettes.js

---
+++ modules/palettes.js
@@ -0,0 +1,39 @@
+const warm = ["#FF2000", "#FF5900", "#FE9100", "#FEFDFC", "#FEC194", "#FE9F5B"];
+const natural = [
+  "#FF6D00",
+  "#FBF8EB",
+  "#008B99",
+  "#F8E1A6",
+  "#FDA81F",
+  "#B80A01",
+  "#480D07",
+];
+const natural2 = [
+  "#EF2006",
+  "#350000",
+  "#A11104",
+  "#ED5910",
+  "#F1B52E",
+  "#7B5614",
+  "#F7F1AC",
+];
+const circus = [
+  "#F62D62",
+  "#FFFFFF",
+  "#FDB600",
+  "#F42D2D",
+  "#544C98",
+  "#ECACBC",
+];
+const seaside = [
+  "#FEB019",
+  "#F46002",
+  "#E1E7F1",
+  "#0A1D69",
+  "#138FE2",
+  "#0652C4",
+  "#D23401",
+  "#B0A12F",
+];
+
+export { warm, natural, natural2, circus, seaside };
