0d92f3b453a87a5fbc813e0d507eeab5257f7483
thespite <the.spite@gmail.com>
2021-11-07 01:02:17 +0000

    Create noise.js

---
+++ shaders/noise.js
@@ -0,0 +1,22 @@
+// https://www.shadertoy.com/view/lsf3WH
+
+const shader = `
+float hash(vec2 p) {
+  p  = 50.0*fract( p*0.3183099 + vec2(0.71,0.113));
+  return -1.0+2.0*fract( p.x*p.y*(p.x+p.y) );
+}
+
+float noise( in vec2 p ) {
+  vec2 i = floor( p );
+  vec2 f = fract( p );
+
+vec2 u = f*f*(3.0-2.0*f);
+
+  return mix( mix( hash( i + vec2(0.0,0.0) ), 
+                   hash( i + vec2(1.0,0.0) ), u.x),
+              mix( hash( i + vec2(0.0,1.0) ), 
+                   hash( i + vec2(1.0,1.0) ), u.x), u.y);
+}
+`;
+
+export { shader };
