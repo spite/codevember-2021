772a7c4c3e91e40618bd734900894cbf00a3f2d3
thespite <the.spite@gmail.com>
2021-11-11 18:02:31 +0000

    Update screen.js

--- shaders/screen.js
+++ shaders/screen.js
@@ -3,6 +3,12 @@ vec4 screen(vec4 base, vec4 blend, float opacity) {
   vec4 color = 1. - (1.-base) * (1.-blend);
   color = color * opacity + base * ( 1. - opacity );
   return color;
+}
+
+vec3 screen(vec3 base, vec3 blend, float opacity) {
+  vec3 color = 1. - (1.-base) * (1.-blend);
+  color = color * opacity + base * ( 1. - opacity );
+  return color;
 }`;
 
 export { shader };
