c6397316c21eaa8421ba125b92e1ec82dea6d8a1
thespite <the.spite@gmail.com>
2021-11-26 19:13:12 +0000

    added 26

---
+++ 26/main.js
@@ -0,0 +1,185 @@
+import {
+  scene,
+  controls,
+  renderer,
+  addResize,
+  resize,
+  camera,
+} from "../modules/renderer.js";
+import {
+  Vector2,
+  Vector3,
+  PlaneBufferGeometry,
+  MeshBasicMaterial,
+  Raycaster,
+  DataTexture,
+  RGBFormat,
+  Mesh,
+  Matrix4,
+} from "../third_party/three.module.js";
+import { Simulation } from "./simulation.js";
+import { GradientLinear } from "../modules/gradient-linear.js";
+import {
+  natural,
+  natural2,
+  circus,
+  circus2,
+  warm2,
+  seaside,
+  warm3,
+} from "../modules/palettes.js";
+import { randomInRange } from "../modules/Maf.js";
+import { Post } from "./post.js";
+// import { capture } from "../modules/capture.js";
+
+const post = new Post(renderer);
+
+const palettes = [natural, natural2, circus, circus2, warm2, seaside, warm3];
+
+function randomize() {
+  const palette = palettes[Math.floor(Math.random() * palettes.length)];
+  const gradient = new GradientLinear(palette);
+
+  const data = new Uint8Array(palette.length * 3);
+  for (let i = 0; i < palette.length; i++) {
+    const c = gradient.getAt(i / (palette.length - 1));
+    data[i * 3] = c.r * 255;
+    data[i * 3 + 1] = c.g * 255;
+    data[i * 3 + 2] = c.b * 255;
+  }
+  const colorTexture = new DataTexture(data, palette.length, 1, RGBFormat);
+  simulation.shader.uniforms.gradient.value = colorTexture;
+  simulation.simShader.uniforms.persistence.value = randomInRange(1, 2.5);
+  simulation.simShader.uniforms.signDir.value = Math.random() > 0.5 ? -1 : 1;
+  simulation.simShader.uniforms.noiseScale.value = randomInRange(0.5, 2);
+  simulation.simShader.uniforms.noiseSpeed.value = randomInRange(0.5, 2);
+  simulation.simShader.uniforms.sphereRadius.value = randomInRange(0.25, 1);
+}
+
+const simulation = new Simulation(1024, 1024);
+scene.add(simulation.mesh);
+randomize();
+
+let prevTime = performance.now();
+let frames = 0;
+let capturing = false;
+
+let running = true;
+let shuffling = true;
+
+const hitPlane = new Mesh(
+  new PlaneBufferGeometry(100, 100),
+  new MeshBasicMaterial({ color: 0x202020 })
+);
+hitPlane.visible = false;
+scene.add(hitPlane);
+
+const raycaster = new Raycaster();
+const mouse = new Vector2();
+const point = new Vector3(0, 0, 0);
+const curPoint = new Vector3(0, 0, 0);
+
+function hitTest() {
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(hitPlane);
+
+  if (intersects.length) {
+    point.copy(intersects[0].point);
+  } else {
+    point.set(0, 0, 0);
+  }
+}
+
+function onPointer(e) {
+  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+}
+
+window.addEventListener("pointermove", (e) => {
+  onPointer(e);
+  hitTest();
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+window.addEventListener("pointerdown", (e) => {
+  onPointer(e);
+  hitTest();
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+function toggleRunning() {
+  running = !running;
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    toggleRunning();
+  }
+  if (e.code === "KeyR") {
+    randomize();
+  }
+  if (e.code === "KeyS") {
+    shuffling = !shuffling;
+  }
+});
+
+document.querySelector("#pauseBtn").addEventListener("click", (e) => {
+  toggleRunning();
+});
+
+document.querySelector("#randomizeBtn").addEventListener("click", (e) => {
+  randomize();
+});
+
+document.querySelector("#autoBtn").addEventListener("click", (e) => {
+  shuffling = !shuffling;
+});
+
+const rot = new Matrix4();
+const tmp = new Vector3();
+
+function render() {
+  hitPlane.lookAt(camera.position);
+
+  const t = performance.now();
+  const dt = (t - prevTime) / 1000;
+  prevTime = t;
+
+  if (running) {
+    curPoint.lerp(point, 0.1);
+    simulation.mesh.rotation.y += dt / 10;
+    rot.makeRotationY(-simulation.mesh.rotation.y);
+    tmp.copy(curPoint);
+    simulation.simulate(renderer, 0.01, tmp.applyMatrix4(rot));
+  }
+
+  // renderer.render(scene, camera);
+  post.render(scene, camera);
+  // capture(renderer.domElement);
+
+  frames++;
+  if (frames > 120 && running && shuffling) {
+    frames = 0;
+    randomize();
+  }
+
+  renderer.setAnimationLoop(render);
+}
+
+function myResize(w, h) {
+  post.setSize(w, h);
+}
+addResize(myResize);
+
+renderer.setClearColor(0x101010, 1);
+resize();
+render();
+
+// window.start = () => {
+//   frames = 0;
+//   capturing = true;
+//   prevTime = performance.now();
+//   window.capturer.start();
+// };
---
+++ 26/simulation.js
@@ -0,0 +1,337 @@
+import { randomInRange } from "../modules/Maf.js";
+import {
+  DataTexture,
+  RGBFormat,
+  RawShaderMaterial,
+  RGBAFormat,
+  GLSL3,
+  AdditiveBlending,
+  BufferGeometry,
+  BufferAttribute,
+  Points,
+  NearestFilter,
+  FloatType,
+  Scene,
+  OrthographicCamera,
+  PlaneBufferGeometry,
+  Mesh,
+  Quaternion,
+  Vector3,
+} from "../third_party/three.module.js";
+import { getFBO } from "../modules/fbo.js";
+import { shader as curl } from "../shaders/curl.js";
+
+const particleVs = `precision highp float;
+
+in vec3 position;
+
+uniform sampler2D positions;
+uniform sampler2D velocities;
+uniform sampler2D gradient;
+uniform float dpr;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+out vec3 vColor;
+out float vLife;
+
+float parabola(in float x, in float k) {
+  return pow(4. * x * (1. - x), k);
+}
+
+void main() {
+  vec2 coord = position.xy;
+  vec4 pos = texture(positions, coord);
+  vec4 vel = texture(velocities, coord);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos.xyz - .5, 1. );
+  vLife = pos.w / 100.;
+  vColor = texture(gradient, vec2(vLife, 0.)).rgb * vLife; 
+  gl_PointSize = 2.5 * dpr / gl_Position.z;
+}`;
+
+const particleFs = `precision highp float;
+
+in float vLife;
+in vec3 vColor;
+
+uniform float opacity;
+
+out vec4 fragColor;
+
+void main() {
+  vec2 circCoord = 2.0 * gl_PointCoord - 1.0;
+  float d = dot(circCoord, circCoord);
+  if (d > 1.0) {
+      discard;
+  }
+  vec3 c = vColor;
+  fragColor = vec4(c, (1.-pow(d, 10.)) * opacity);
+}`;
+
+const simVs = `precision  highp float;
+
+in vec3 position;
+in vec2 uv;
+
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+
+out vec2 vUv;
+
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+}`;
+
+const simFs = `precision highp float;
+precision highp sampler3D;
+
+in vec2 vUv;
+
+uniform sampler2D originalPos;
+uniform sampler2D posTexture;
+uniform sampler2D velTexture;
+uniform bool positions;
+uniform float dt;
+uniform float noiseSpeed;
+uniform float noiseScale;
+uniform vec3 mousePosition;
+uniform float modulo;
+uniform float persistence;
+uniform float time;
+uniform float sphereRadius;
+uniform float signDir;
+
+out vec4 fragColor;
+
+float sdSphere(in vec3 p, in float r) {
+  return length(p)-r;
+}
+
+${curl}
+
+void main() {
+  if(positions == true) {
+    vec4 pos = texture(posTexture, vUv);
+    vec4 vel = texture(velTexture, vUv);
+       
+    pos.xyz = pos.xyz + vel.xyz * vel.w * dt;
+    pos.w += vel.w * dt * 4.;
+    
+    if(pos.w>100.) {
+      pos.w = 0.;
+      pos.xyz = texture(originalPos, vUv).xyz + mousePosition + .5;
+    }
+    
+    fragColor = pos; 
+  } else {
+    vec4 pos = texture(posTexture, vUv);
+    vec4 vel = texture(velTexture, vUv);
+    
+    if(pos.w== 0.) {
+      vel.xyz = vec3(0.);
+    }
+
+    vec2 size = vec2(textureSize(posTexture, 0));
+    if(mod(round(vUv.x * size.x), 4.) == modulo) {
+      float d = sdSphere((pos.xyz - .5)-mousePosition.xyz, sphereRadius);
+      vel.xyz = noiseSpeed * curlNoise(noiseScale * pos.xyz / 2., time/10.) / 200.;
+
+      if(d<0.) {
+        vec3 n = normalize(pos.xyz - .5 - mousePosition.xyz);
+        vec3 r = signDir * refract(n, normalize(vel.xyz), .85);
+        vel.xyz += r/10. * vel.w;
+      } else {
+        vel.xyz /= 1. + pow(d, 2.);
+      }
+    }
+
+    fragColor = vel;
+  }
+}`;
+
+function getPoint(radius) {
+  const u = Math.random();
+  const v = Math.random();
+  const theta = u * 2.0 * Math.PI;
+  const phi = Math.acos(2.0 * v - 1.0);
+  const r = radius * Math.cbrt(Math.random());
+  const sinTheta = Math.sin(theta);
+  const cosTheta = Math.cos(theta);
+  const sinPhi = Math.sin(phi);
+  const cosPhi = Math.cos(phi);
+  const x = r * sinPhi * cosTheta;
+  const y = r * sinPhi * sinTheta;
+  const z = r * cosPhi;
+  return { x: x, y: y, z: z };
+}
+
+class Simulation {
+  constructor(width, height) {
+    const posData = new Float32Array(width * height * 4);
+    let ptr = 0;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const p = getPoint(0.5);
+        posData[ptr] = p.x;
+        posData[ptr + 1] = p.y;
+        posData[ptr + 2] = p.z;
+        posData[ptr + 3] = randomInRange(0, 100);
+        ptr += 4;
+      }
+    }
+    const posTexture = new DataTexture(
+      posData,
+      width,
+      height,
+      RGBAFormat,
+      FloatType
+    );
+
+    const velData = new Float32Array(width * height * 4);
+    ptr = 0;
+    let r = 0.01;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        velData[ptr] = randomInRange(-r, r);
+        velData[ptr + 1] = randomInRange(-r, r);
+        velData[ptr + 2] = randomInRange(-r, r);
+        velData[ptr + 3] = randomInRange(0.25, 0.5);
+        ptr += 4;
+      }
+    }
+    const velTexture = new DataTexture(
+      velData,
+      width,
+      height,
+      RGBAFormat,
+      FloatType
+    );
+
+    this.shader = new RawShaderMaterial({
+      uniforms: {
+        positions: { value: posTexture },
+        velocities: { value: velTexture },
+        gradient: { value: null },
+        dpr: { value: 1 },
+        opacity: { value: 0.5 },
+      },
+      vertexShader: particleVs,
+      fragmentShader: particleFs,
+      glslVersion: GLSL3,
+      transparent: true,
+      depthWrite: false,
+      depthTest: true,
+      blending: AdditiveBlending,
+    });
+    const geo = new BufferGeometry();
+    const vertices = new Float32Array(width * height * 3);
+    ptr = 0;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        vertices[ptr] = x / width;
+        vertices[ptr + 1] = y / width;
+        vertices[ptr + 2] = 0;
+        ptr += 3;
+      }
+    }
+    geo.setAttribute("position", new BufferAttribute(vertices, 3));
+    this.mesh = new Points(geo, this.shader);
+
+    const options = {
+      format: RGBAFormat,
+      type: FloatType,
+      minFilter: NearestFilter,
+      magFilter: NearestFilter,
+    };
+
+    this.positionFBOs = [
+      getFBO(width, height, options),
+      getFBO(width, height, options),
+    ];
+    this.velocityFBOs = [
+      getFBO(width, height, options),
+      getFBO(width, height, options),
+    ];
+
+    this.simShader = new RawShaderMaterial({
+      uniforms: {
+        originalPos: { value: posTexture },
+        posTexture: { value: posTexture },
+        velTexture: { value: velTexture },
+        mousePosition: { value: new Vector3() },
+        signDir: { value: 1 },
+        dt: { value: 1 },
+        noiseScale: { value: 1 },
+        persistence: { value: 1 },
+        noiseSpeed: { value: 1 },
+        positions: { value: false },
+        time: { value: 0 },
+        sphereRadius: { value: 0.5 },
+        modulo: { value: 0 },
+      },
+      vertexShader: simVs,
+      fragmentShader: simFs,
+      glslVersion: GLSL3,
+      depthTest: false,
+      depthWrite: false,
+    });
+
+    this.simScene = new Scene();
+    this.simCamera = new OrthographicCamera(
+      -width / 2,
+      width / 2,
+      height / 2,
+      -height / 2,
+      0.00001,
+      1000
+    );
+    const simQuad = new Mesh(new PlaneBufferGeometry(1, 1), this.simShader);
+    simQuad.scale.set(width, height, 1);
+    this.simScene.add(simQuad);
+
+    this.current = 0;
+  }
+
+  simulate(renderer, dt, point) {
+    this.simShader.uniforms.mousePosition.value.copy(point);
+    this.simShader.uniforms.time.value += dt;
+    this.simShader.uniforms.dt.value = dt / 0.016;
+    this.shader.uniforms.dpr.value = renderer.getPixelRatio();
+
+    this.simShader.uniforms.positions.value = false;
+    renderer.setRenderTarget(this.velocityFBOs[1 - this.current]);
+    renderer.render(this.simScene, this.simCamera);
+    renderer.setRenderTarget(null);
+
+    this.simShader.uniforms.velTexture.value =
+      this.velocityFBOs[1 - this.current].texture;
+
+    this.simShader.uniforms.positions.value = true;
+    renderer.setRenderTarget(this.positionFBOs[1 - this.current]);
+    renderer.render(this.simScene, this.simCamera);
+    renderer.setRenderTarget(null);
+
+    this.simShader.uniforms.posTexture.value =
+      this.positionFBOs[1 - this.current].texture;
+
+    this.shader.uniforms.positions.value =
+      this.positionFBOs[1 - this.current].texture;
+    this.shader.uniforms.velocities.value =
+      this.velocityFBOs[1 - this.current].texture;
+    //debug.material.map = this.positionFBOs[1 - this.current].texture;
+
+    this.simShader.uniforms.modulo.value =
+      (this.simShader.uniforms.modulo.value + 1) % 4;
+    this.current = 1 - this.current;
+  }
+}
+
+// const debug = new Mesh(
+//   new PlaneBufferGeometry(1, 1),
+//   new MeshBasicMaterial({ map: null })
+// );
+// scene.add(debug);
+
+export { Simulation };
