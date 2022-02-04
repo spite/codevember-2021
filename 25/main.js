7d0a0d413883db2f8786553c4d2d0470cb3fbbef
thespite <the.spite@gmail.com>
2022-02-04 21:42:24 +0000

    fix post size

--- 25/main.js
+++ 25/main.js
@@ -275,9 +275,9 @@ function render() {
   renderer.setAnimationLoop(render);
 }
 
-addResize((w, h) => {
-  ssao.setSize(w, h);
-  post.setSize(w, h);
+addResize((w, h, dpr) => {
+  ssao.setSize(w * dpr, h * dpr);
+  post.setSize(w * dpr, h * dpr);
 });
 
 camera.position.set(0, 0, 3);
